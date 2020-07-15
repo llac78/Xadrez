811e209395caef2779441a5e1d522a7d0eecfe85
llac78 <llac78@gmail.com>
2020-07-15 11:44:38 -0300

    Início da implementação do Tabuleiro e Peça

---
+++ src/tabuleiro/Peca.java
@@ -0,0 +1,19 @@
+package tabuleiro;
+
+public class Peca {
+	
+	private Posicao posicaoMatriz;
+	protected Tabuleiro tabuleiro;
+
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+
+	public Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+	
+	
+	
+
+}
