c8f430947e5616d8616ca6b4b1eef2b8570bd654
llac78 <llac78@gmail.com>
2020-07-22 16:34:29 -0300

    Espaçamento das coordenadas do tabuleiro

--- src/aplicacao/UI.java
+++ src/aplicacao/UI.java
@@ -54,25 +54,27 @@ public class UI {
 	public static void imprimirTabuleiro(PecaXadrez[][] pecas) {
 
 		for (int i = 0; i < pecas.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print((8 - i) + "  ");	
 			for (int j = 0; j < pecas.length; j++) {
 				imprimirPeca(pecas[i][j], false);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println(" ");
+		System.out.println("   a b c d e f g h");
 	}
 	
 	public static void imprimirTabuleiro(PecaXadrez[][] pecas, boolean[][] movimentosPossiveis) {
 
 		for (int i = 0; i < pecas.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print((8 - i) + "  ");
 			for (int j = 0; j < pecas.length; j++) {
 				imprimirPeca(pecas[i][j], movimentosPossiveis[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");
+		System.out.println(" ");
+		System.out.println("   a b c d e f g h");
 	}
 
 	private static void imprimirPeca(PecaXadrez peca, boolean background) {
